444983b5da1421fd35ae9795d27143f705e832e7
caishenao <s7wfvkn@dingtalk.com>
2023-07-23 10:40:43 +0800

    init

---
+++ src/main/java/cn/cai/getway/GetwayApplication.java
@@ -0,0 +1,16 @@
+package cn.cai.getway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GetwayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GetwayApplication.class, args);
+    }
+
+}
